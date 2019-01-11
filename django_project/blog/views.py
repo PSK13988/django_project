9fc9f78068a5fe9986969b04994f6783021c2c11
psk13988 <pankajkharche13@gmail.com>
2019-01-11 14:20:27 +0530

    user app added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-posts=[{
-    'author':'Pankaj Kharche',
-    'title':'First Blog post',
-    'content':'This is my First Blog message',
-    'date_posted':'January 10, 2019'
-},{
-    'author':'Jagruti Kharche',
-    'title':'Second Blog post',
-    'content':'This is my Second Blog message',
-    'date_posted':'January 11, 2019'
-}]
-
+from .models import Post
 
 def home(request):
-    return render(request, 'blog/home.html', {'title':'Pankaj Kharche', 'posts': posts})
+    return render(request, 'blog/home.html', {'title':'Pankaj Kharche', 'posts': Post.objects.all()})
 
 def about(request):
     return render(request, 'blog/about.html', {})
