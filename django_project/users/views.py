f9d8bb51acde159413b0dae7e7062d5c03eb7abd
psk13988 <pankajkharche13@gmail.com>
2019-01-11 17:08:56 +0530

    register issue fixed

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -8,9 +8,9 @@ def register(request):
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
+            # username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
-            redirect(request, 'blog-home')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html',{'form':form})
